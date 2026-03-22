dfd41e830625222ec31bed7b61b9bee19c79a2a8
xiaricli <xiaricli@gmail.com>
2026-03-22 14:49:09 -0600

    Actualizar archivos del proyecto después de agregar .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/anithuncommon/',
-
+  base: '/anithuncommon/',  //
   plugins: [
     react(),
     tailwindcss(),
